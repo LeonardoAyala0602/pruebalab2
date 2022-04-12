529a19737afb3992728594c7b649e1f163845c84
Joau0824 <a20182162@pucp.edu.pe>
2022-04-11 23:48:55 -0500

    nuevo proyecto

--- src/main/java/com/example/pruebalab2/controller/ProyectoController.java
+++ src/main/java/com/example/pruebalab2/controller/ProyectoController.java
@@ -2,7 +2,9 @@ package com.example.pruebalab2.controller;
 
 
 import com.example.pruebalab2.entity.Proyecto;
+import com.example.pruebalab2.entity.UsuarioEntity;
 import com.example.pruebalab2.repository.ProyectoRepository;
+import com.example.pruebalab2.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +21,20 @@ public class ProyectoController {
     @Autowired
     ProyectoRepository proyectoRepository;
 
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
     @GetMapping("/listar")
     public String listarProyecto(Model model){
         List<Proyecto> proyectoList = proyectoRepository.findAll();
         model.addAttribute("proyectoList", proyectoList);
         return "proyecto/lista";
+    }
+
+    @GetMapping("/newform")
+    public String newform(Model model){
+        List<UsuarioEntity> usuarioEntityList = usuarioRepository.findAll();
+        model.addAttribute("usuarioEntityList",usuarioEntityList);
+        return "proyecto/newform";
     }
 }
