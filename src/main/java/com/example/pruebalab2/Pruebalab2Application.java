b007fcd04fc9cba50a2fd0c2811c380a0196d37a
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-04-11 20:46:56 -0500

    Proyecto Base

---
+++ src/main/java/com/example/pruebalab2/Pruebalab2Application.java
@@ -0,0 +1,13 @@
+package com.example.pruebalab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pruebalab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pruebalab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruebalab2/Pruebalab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebalab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pruebalab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
