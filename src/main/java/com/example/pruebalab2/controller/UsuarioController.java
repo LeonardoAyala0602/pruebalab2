698e0aed8c181285fb80985f99403766ea51bd09
stephaniaguzman <stephania.guzman@pucp.edu.pe>
2022-04-12 18:57:44 -0500

    Usuario

--- src/main/java/com/example/pruebalab2/controller/UsuarioController.java
+++ src/main/java/com/example/pruebalab2/controller/UsuarioController.java
@@ -1,14 +1,19 @@
 package com.example.pruebalab2.controller;
 
+import com.example.pruebalab2.entity.AreaEntity;
 import com.example.pruebalab2.entity.UsuarioEntity;
+import com.example.pruebalab2.repository.AreaRepository;
 import com.example.pruebalab2.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -17,10 +22,47 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    AreaRepository areaRepository;
+
+
     @GetMapping("/listar")
     public String listarUsuarios(Model model){
         List<UsuarioEntity> listaUsuarios = usuarioRepository.findAll();
         model.addAttribute("listaUsuarios",listaUsuarios);
         return "usuario/lista";
+    }
+
+    @GetMapping("/newform")
+    public String newformUsuarios(Model model){
+        List<AreaEntity> listaAreas = areaRepository.findAll();
+        model.addAttribute("listaAreas",listaAreas);
+        return "usuario/newform";
+    }
+
+    @PostMapping("/save")
+    public String saveUsuarios(UsuarioEntity usuarioEntity){
+        usuarioRepository.save(usuarioEntity);
+        return "redirect:/usuario/listar";
+    }
+
+    @GetMapping("/editar")
+    public String editarUsuarios(@RequestParam("correo") String correo, Model model){
+        Optional<UsuarioEntity> optionalUsuario = usuarioRepository.findById(correo);
+        if(optionalUsuario.isPresent()){
+            UsuarioEntity usuario = optionalUsuario.get();
+            model.addAttribute("usuario", usuario);
+            List<AreaEntity> listaAreas = areaRepository.findAll();
+            model.addAttribute("listaAreas",listaAreas);
+            return "usuario/editar";
+        }else{
+            return "redirect:/usuario/listar";}
+    }
+
+    @GetMapping("/borrar")
+    public String borrarUsuarios(@RequestParam("correo") String correo){
+        Optional<UsuarioEntity> optionalUsuario = usuarioRepository.findById(correo);
+            usuarioRepository.deleteById(correo);
+        return "redirect:/usuario/listar";
     }
 }
--- src/main/java/com/example/pruebalab2/repository/AreaRepository.java
+++ src/main/java/com/example/pruebalab2/repository/AreaRepository.java
@@ -1,10 +1,17 @@
 package com.example.pruebalab2.repository;
 
 import com.example.pruebalab2.entity.AreaEntity;
+import com.example.pruebalab2.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AreaRepository extends JpaRepository<AreaEntity,Integer> {
+
+    @Query(value = "select * from areas",nativeQuery = true)
+    List<AreaEntity> listaAreas(int idarea);
 
 }
